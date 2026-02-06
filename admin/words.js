e00654f5d0fc7795953736630ee70396158eb976
Felix Pachmann <f.pachmann@nedeco.de>
2026-02-06 15:41:56 +0100

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,46 @@
+/* eslint no-unused-vars: off */
+/* eslint no-global-assign: off */
+/* global systemDictionary */
+"use strict";
+
+systemDictionary = {
+	"open-smart-city-home adapter settings": {
+		"en": "Adapter settings for open-smart-city-home",
+		"de": "Adaptereinstellungen für open-smart-city-home",
+		"ru": "Настройки адаптера для open-smart-city-home",
+		"pt": "Configurações do adaptador para open-smart-city-home",
+		"nl": "Adapterinstellingen voor open-smart-city-home",
+		"fr": "Paramètres d'adaptateur pour open-smart-city-home",
+		"it": "Impostazioni dell'adattatore per open-smart-city-home",
+		"es": "Ajustes del adaptador para open-smart-city-home",
+		"pl": "Ustawienia adaptera dla open-smart-city-home",
+		"uk": "Налаштування адаптера для open-smart-city-home",
+		"zh-cn": "open-smart-city-home的适配器设置"
+	},
+	"option1": {
+		"en": "option1",
+		"de": "Option1",
+		"ru": "вариант 1",
+		"pt": "opção1",
+		"nl": "optie1",
+		"fr": "option1",
+		"it": "opzione1",
+		"es": "opción1",
+		"pl": "opcja 1",
+		"uk": "варіант1",
+		"zh-cn": "选项1"
+	},
+	"option2": {
+		"en": "option2",
+		"de": "Option2",
+		"ru": "вариант 2",
+		"pt": "opção2",
+		"nl": "optie2",
+		"fr": "option2",
+		"it": "opzione2",
+		"es": "opción2",
+		"pl": "opcja 2",
+		"uk": "варіант2",
+		"zh-cn": "选项2"
+	}
+};
